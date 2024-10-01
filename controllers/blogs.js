01f3a6b869e46edd6e9cc3a68b6d3e4630a7b00f
ygyesus <yosefgyesus.in@gmail.com>
2024-10-01 08:49:27 +0300

    Do 4.8

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,22 +1,26 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 
-blogsRouter.get('/', (req, res) => {
-    Blog
+blogsRouter.get('/', async (req, res) => {
+   await Blog
       .find({})
       .then(blogs => {
         res.json(blogs)
       })
 })
 
-blogsRouter.post('/', (req, res) => {
+blogsRouter.post('/', async (req, res) => {
 
     const blog = new Blog(req.body)
 
-    blog.save()
+    await blog.save()
         .then(result => {
             res.status(201).json(result)
         })
 })
 
+blogsRouter.delete('/', async(req, res) => {
+  await Blog.deleteMany({})
+  res.status(204).end()
+})
 module.exports = blogsRouter
